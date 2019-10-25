c4686840082e23159f127cb6840219395ed761c1
hobo0xcc <hobo0xcc@gmail.com>
2019-10-26 03:32:01 +0900

    Add Parser.

--- src/main.rs
+++ src/main.rs
@@ -14,42 +14,7 @@ use inkwell::values::{FloatValue, FunctionValue};
 use std::error::Error;
 use std::fmt;
 
-struct Compiler<'a> {
-    context: &'a Context,
-    module: &'a Module,
-    builder: &'a Builder,
-    execution_engine: &'a ExecutionEngine,
-}
-
-type Sum = unsafe extern "C" fn () -> f64;
-
-impl<'a> Compiler<'a> {
-    pub fn new(context: &'a Context,
-               module: &'a Module,
-               builder: &'a Builder,
-               execution_engine: &'a ExecutionEngine) -> Self {
-        let mut compiler = Compiler {
-            context,
-            module,
-            builder,
-            execution_engine,
-        };
-        compiler.create_func("Sum");
-        compiler.builder.build_alloca(compiler.context.f64_type(), "sumval");
-        compiler
-    }
-
-    pub fn create_func(&mut self, name: &'a str) -> FunctionValue {
-        let f64_type = self.context.f64_type();
-        let fn_type = f64_type.fn_type(&[], false);
-        let func = self.module.add_function(name, fn_type, None);
-        let basic_block = self.context.append_basic_block(&func, "entry");
-        self.builder.position_at_end(&basic_block);
-
-        func
-    }
-}
-
+#[derive(Debug, Clone, PartialEq)]
 pub enum Token {
     Number(f64),    // [0-9]+
     Ident(String),  // [a-zA-Z_]+
@@ -260,70 +225,199 @@ impl Iterator for Lexer {
     }
 }
 
-fn primary_expr(compiler: &Compiler, p: &Vec<(Token, u32)>, pc: &mut usize) -> FloatValue {
-    *pc += 1;
-    match p[*pc - 1].0 {
-        Token::Number(n) => compiler.context.f64_type().const_float(n),
-        Token::LParen => {
-            let exp = add_expr(compiler, p, pc);
-            *pc += 1;
-            match p[*pc - 1].0 {
-                Token::RParen => return exp,
-                _ => {
-                    eprintln!("Error (at line {}): Parentheses are not closed.", p[*pc].1);
+pub enum AST {
+    BinaryExpr {
+        lhs: Box<AST>,
+        op: Token,
+        rhs: Box<AST>,
+    },
+    Number(f64),
+}
+
+impl fmt::Display for AST {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let fmt_str = match self {
+            AST::BinaryExpr { lhs, op, rhs } => {
+                format!("binary({} {} {})", lhs, op, rhs)
+            },
+            AST::Number(n) => {
+                format!("number({})", n)
+            }
+        };
+        write!(f, "{}", fmt_str)
+    }
+}
+
+pub struct Parser {
+    tokens: Vec<(Token, u32)>,
+    p: usize,
+}
+
+impl Parser {
+    pub fn new(src: Vec<u8>) -> Self {
+        let lex = Lexer::new(src);
+        let tokens: Vec<(Token, u32)> = lex.collect();
+
+        Parser {
+            tokens,
+            p: 0,
+        }
+    }
+
+    pub fn eat(&mut self) -> (Token, u32) {
+        self.p += 1;
+        if let Some(t) = self.tokens.get(self.p - 1) {
+            t.clone()
+        } else {
+            (Token::EOF, 0)
+        }
+    }
+
+    pub fn cur(&self) -> (Token, u32) {
+        if let Some(t) = self.tokens.get(self.p) {
+            t.clone()
+        } else {
+            (Token::EOF, 0)
+        }
+    }
+
+    pub fn peek(&self, i: usize) -> (Token, u32) {
+        if let Some(t) = self.tokens.get(self.p + i) {
+            t.clone()
+        } else {
+            (Token::EOF, 0)
+        }
+    }
+
+    pub fn primary_expr(&mut self) -> AST {
+        match self.eat() {
+            (Token::LParen, _) => {
+                let expr = self.add_expr();
+                if self.cur().0 != Token::RParen {
+                    eprintln!("Error (at line {}): Parentheses are not closed.", self.cur().1);
                     process::exit(1);
                 }
+                self.p += 1;
+                expr
+            },
+            (Token::Number(n), _) => AST::Number(n),
+            (tok, line) => {
+                eprintln!("Error (at line {}): Unexpected Token: {}", line, tok);
+                process::exit(1);
             }
-        },
-        _ => {
-            eprintln!("Error (at line {}): Unknown Token: {}", p[*pc].1, p[*pc].0);
-            process::exit(1);
         }
+    }
+
+    pub fn mul_expr(&mut self) -> AST {
+        let mut left = self.primary_expr();
+        loop {
+            let op = self.cur();
+            match op {
+                (Token::AsterRisk, _) | (Token::Slash, _) => self.p += 1,
+                _ => break,
+            }
+
+            let right = self.primary_expr();
+            match op.0 {
+                Token::AsterRisk | Token::Slash => {
+                    left = AST::BinaryExpr {
+                        lhs: Box::new(left),
+                        op: op.0.clone(),
+                        rhs: Box::new(right) }
+                },
+                _ => break,
+            }
+        }
+
+        left
+    }
+
+    pub fn add_expr(&mut self) -> AST {
+        let mut left = self.mul_expr();
+        loop {
+            let op = self.cur();
+            match op {
+                (Token::Plus, _) | (Token::Minus, _) => self.p += 1,
+                _ => break,
+            }
+
+            let right = self.mul_expr();
+            match op.0 {
+                Token::Plus | Token::Minus => {
+                    left =  AST::BinaryExpr {
+                        lhs: Box::new(left),
+                        op: op.0.clone(),
+                        rhs: Box::new(right) }
+                },
+                _ => break,
+            }
+        }
+
+        left
+    }
+
+    pub fn parse(&mut self) -> AST {
+        self.add_expr()
     }
 }
 
-fn mul_expr(compiler: &Compiler, p: &Vec<(Token, u32)>, pc: &mut usize) -> FloatValue {
-    let mut left = primary_expr(compiler, p, pc);
-    let mut op: Token;
-    while p.len() > *pc {
-        match p[*pc].0 {
-            Token::AsterRisk    => op = Token::AsterRisk,
-            Token::Slash        => op = Token::Slash,
-            _ => break,
-        }
-        *pc += 1;
-
-        let right = primary_expr(compiler, p, pc);
-        match op {
-            Token::AsterRisk    => left = compiler.builder.build_float_mul(left, right, "sumval"),
-            Token::Slash        => left = compiler.builder.build_float_div(left, right, "sumval"),
-            _ => break,
-        }
-    }
-
-    left
+struct Compiler<'a> {
+    context: &'a Context,
+    module: &'a Module,
+    builder: &'a Builder,
+    execution_engine: &'a ExecutionEngine,
 }
 
-fn add_expr(compiler: &Compiler, p: &Vec<(Token, u32)>, pc: &mut usize) -> FloatValue {
-    let mut left = mul_expr(compiler, p, pc);
-    let mut op: Token;
-    while p.len() > *pc {
-        match p[*pc].0 {
-            Token::Plus     => op = Token::Plus,
-            Token::Minus    => op = Token::Minus,
-            _ => break,
-        }
-        *pc += 1;
+type Sum = unsafe extern "C" fn () -> f64;
 
-        let right = mul_expr(compiler, p, pc);
-        match op {
-            Token::Plus     => left = compiler.builder.build_float_add(left, right, "sumval"),
-            Token::Minus    => left = compiler.builder.build_float_sub(left, right, "sumval"),
-            _ => break,
+impl<'a> Compiler<'a> {
+    pub fn new(context: &'a Context,
+               module: &'a Module,
+               builder: &'a Builder,
+               execution_engine: &'a ExecutionEngine) -> Self {
+        let mut compiler = Compiler {
+            context,
+            module,
+            builder,
+            execution_engine,
+        };
+        compiler.create_func("Sum");
+        compiler.builder.build_alloca(compiler.context.f64_type(), "sumval");
+        compiler
+    }
+
+    pub fn create_func(&mut self, name: &'a str) -> FunctionValue {
+        let f64_type = self.context.f64_type();
+        let fn_type = f64_type.fn_type(&[], false);
+        let func = self.module.add_function(name, fn_type, None);
+        let basic_block = self.context.append_basic_block(&func, "entry");
+        self.builder.position_at_end(&basic_block);
+
+        func
+    }
+
+    pub fn compile_expr(&mut self, expr: AST) -> FloatValue {
+        match expr {
+            AST::Number(n) => {
+                self.context.f64_type().const_float(n)
+            },
+            AST::BinaryExpr { lhs, op, rhs } => {
+                let left = self.compile_expr(*lhs);
+                let right = self.compile_expr(*rhs);
+                match op {
+                    Token::Plus => self.builder.build_float_add(left, right, "sumval"),
+                    Token::Minus => self.builder.build_float_sub(left, right, "sumval"),
+                    Token::AsterRisk => self.builder.build_float_mul(left, right, "sumval"),
+                    Token::Slash => self.builder.build_float_div(left, right, "sumval"),
+                    _ => left
+                }
+            }
         }
     }
 
-    left
+    pub fn compile(&mut self, ast: AST) -> FloatValue {
+        self.compile_expr(ast)
+    }
 }
 
 fn jit_compile(compiler: &Compiler, funcname: &str) -> Option<JitFunction<Sum>> {
@@ -339,6 +433,14 @@ fn debug_lexer() {
     for (i, (tok, line)) in tokens.iter().enumerate() {
         println!("[{}]\t(line {}): '{}'", i, line, tok);
     }
+}
+
+#[test]
+fn debug_parser() {
+    let src = String::from("10 + 20 + 30 * 40 / 5").into_bytes();
+    let mut parser = Parser::new(src);
+    let ast = parser.parse();
+    println!("{}", ast);
 }
 
 fn read_file(filename: &str) -> Vec<u8> {
@@ -360,20 +462,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Read file.
     let data = read_file(&argv[1]);
 
-    let lex = Lexer::new(data);
-    let tokens = lex.collect();
+    let mut parser = Parser::new(data);
+    let ast = parser.parse();
 
-    // Counter of tokens.
-    let mut pc: usize = 0;
     // LLVM Settings.
     let context             = Context::create();
     let module              = context.create_module("sum");
     let builder             = context.create_builder();
     let execution_engine    = module.create_jit_execution_engine(OptimizationLevel::None)?;
 
-    let compiler = Compiler::new(&context, &module, &builder, &execution_engine);
+    let mut compiler = Compiler::new(&context, &module, &builder, &execution_engine);
 
-    let sum = add_expr(&compiler, &tokens, &mut pc);
+    let sum = compiler.compile(ast);
     compiler.builder.build_return(Some(&sum));
     let func = jit_compile(&compiler, "Sum").ok_or("Error Occurred.")?;
     unsafe {
